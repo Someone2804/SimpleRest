4a33e2cc92f23517e43cfa7e6c5edc30365c0911
Sergey Orudzhev <ose19988@gmail.com>
2021-09-29 19:07:57 +0300

    Added createPost

--- src/main/java/com/SimpleRest/SimpleRestAPI/controllers/PostController.java
+++ src/main/java/com/SimpleRest/SimpleRestAPI/controllers/PostController.java
@@ -1,28 +1,29 @@
 package com.SimpleRest.SimpleRestAPI.controllers;
 
+import com.SimpleRest.SimpleRestAPI.service.ImageService;
 import com.SimpleRest.SimpleRestAPI.service.PostService;
 import com.SimpleRest.SimpleRestAPI.store.dto.PostDTO;
+import com.SimpleRest.SimpleRestAPI.store.entity.Post;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.ServletContext;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
 @RestController
 @RequestMapping("/posts")
 public class PostController {
     final PostService postService;
+    final ImageService imageService;
 
     @Autowired
-    public PostController(PostService postService) {
+    public PostController(PostService postService, ImageService imageService) {
         this.postService = postService;
+        this.imageService = imageService;
     }
 
     @GetMapping("{id}")
@@ -35,17 +36,13 @@ public class PostController {
         return new ResponseEntity<>(postService.findAll(), HttpStatus.OK);
     }
 
-    @GetMapping("image/{name}")
-    public ResponseEntity<byte[]> getImage(@PathVariable String name) throws IOException {
-        HttpHeaders headers = new HttpHeaders();
-        InputStream in = getClass().getResourceAsStream("/templates/" + name);
-        if(in == null){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        byte[] media = in.readAllBytes();
-        headers.setCacheControl(CacheControl.noCache().getHeaderValue());
-
-        return new ResponseEntity<>(media, headers, HttpStatus.OK);
+    @PostMapping("/create")
+    public ResponseEntity<?> createPost(@RequestPart("images") List<MultipartFile> multipartFile, @RequestPart String head, @RequestPart String text){
+        Post post = new Post();
+        post.setHead(head);
+        post.setText(text);
+        postService.savePost(post, multipartFile);
+        return ResponseEntity.ok(200);
     }
 
     @PreAuthorize("hasRole('ADMIN')")
