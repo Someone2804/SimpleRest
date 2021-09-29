1291a8c693e3730138257a58bdbfae127c4116ca
Sergey Orudzhev <ose19988@gmail.com>
2021-09-29 19:05:05 +0300

    Added imageToByte

--- src/main/java/com/SimpleRest/SimpleRestAPI/service/impl/ImageServiceImpl.java
+++ src/main/java/com/SimpleRest/SimpleRestAPI/service/impl/ImageServiceImpl.java
@@ -3,18 +3,23 @@ package com.SimpleRest.SimpleRestAPI.service.impl;
 import com.SimpleRest.SimpleRestAPI.exceptions.NotFoundException;
 import com.SimpleRest.SimpleRestAPI.service.ImageService;
 import com.SimpleRest.SimpleRestAPI.store.dto.ImageDTO;
-import com.SimpleRest.SimpleRestAPI.store.entity.Image;
 import com.SimpleRest.SimpleRestAPI.store.repo.ImageRepo;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @Service
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ImageServiceImpl implements ImageService {
 
     final ImageRepo imageRepo;
+    @Value("${images.path}")
+    String path;
 
     @Autowired
     public ImageServiceImpl(ImageRepo imageRepo) {
@@ -26,13 +31,27 @@ public class ImageServiceImpl implements ImageService {
         return ImageDTO.imageToImageDTO(imageRepo.findById(id).orElseThrow(() -> new NotFoundException("User id not found")));
     }
 
-    @Override
-    public ImageDTO saveImage(Image image) {
-        return null;
-    }
 
     @Override
     public void deleteImage(long id) {
+        findById(id);
+        imageRepo.deleteById(id);
+    }
+
+    @Override
+    public byte[] imageToByte(String name) {
+        InputStream in = getClass().getResourceAsStream("/templates/" + name);
+        if(in == null){
+            throw new NotFoundException("Image " + name + " not found");
+        }
+        byte[] media = null;
+        try {
+            media = in.readAllBytes();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return media;
 
     }
 }
